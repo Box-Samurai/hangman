c8b627d808c12803db164e96c10132e551b2a28d
Ken Cardona <kencardona@Kens-MacBook-Air.local>
2018-04-06 13:14:25 +0900

    my first commit

---
+++ Hangaman.py
@@ -0,0 +1,406 @@
+Python 3.6.4 (v3.6.4:d48ecebad5, Dec 18 2017, 21:07:28) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: /Users/kencardona/Desktop/Delete.py ================
+Welcome to Hangman!
+
+
+Guess a letter 
+k
+_ _ _ k _ _ _ _ _ _
+
+
+
+Guess a letter 
+n
+_ _ _ k _ _ _ _ _ _
+
+________        
+
+
+Guess a letter 
+b
+_ _ _ k _ _ b _ _ _
+
+________        
+
+
+Guess a letter 
+i
+_ _ _ k _ _ b _ _ _
+
+________        
+|               
+
+
+Guess a letter 
+u
+_ _ _ k _ _ b _ _ _
+
+________        
+|               
+|       |       
+
+
+Guess a letter 
+i
+_ _ _ k _ _ b _ _ _
+
+________        
+|               
+|       |       
+|       O       
+
+
+Guess a letter 
+o
+_ _ _ k _ _ b _ _ _
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+
+
+Guess a letter 
+a
+_ a _ k _ _ b _ _ _
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+
+
+Guess a letter 
+e
+_ a _ k e _ b _ _ _
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+
+
+Guess a letter 
+e
+_ a _ k e _ b _ _ _
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+|      / \      
+
+
+Guess a letter 
+c
+_ a _ k e _ b _ _ _
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+|      / \      
+|               
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+|      / \      
+|               
+You lose buttface! The correct answer was Basketball.
+>>> 
+================ RESTART: /Users/kencardona/Desktop/Delete.py ================
+Welcome to Hangman!
+
+
+Guess a letter 
+d
+_ _ _ _ _ _ _
+
+________        
+
+
+Guess a letter 
+a
+_ _ _ _ _ a _
+
+________        
+
+
+Guess a letter 
+d
+_ _ _ _ _ a _
+
+________        
+|               
+
+
+Guess a letter 
+t
+_ _ _ _ _ a _
+
+________        
+|               
+|       |       
+
+
+Guess a letter 
+o
+_ _ _ _ _ a _
+
+________        
+|               
+|       |       
+|       O       
+
+
+Guess a letter 
+e
+_ _ _ e _ a _
+
+________        
+|               
+|       |       
+|       O       
+
+
+Guess a letter 
+t
+_ _ _ e _ a _
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+
+
+Guess a letter 
+x
+_ _ _ e _ a _
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+|      / \      
+
+
+Guess a letter 
+v
+_ _ _ e _ a _
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+|      / \      
+|               
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+|      / \      
+|               
+You lose buttface! The correct answer was Fireman.
+>>> 
+================ RESTART: /Users/kencardona/Desktop/Delete.py ================
+Welcome to Hangman!
+
+
+Guess a letter 
+d
+_ _ _ _ _ _ _ _ _ _ _ _ _
+
+________        
+
+
+Guess a letter 
+a
+_ _ _ _ _ _ _ _ _ _ _ _ _
+
+________        
+|               
+
+
+Guess a letter 
+i
+_ _ _ _ _ _ _ _ _ _ _ _ _
+
+________        
+|               
+|       |       
+
+
+Guess a letter 
+o
+_ _ _ _ o _ _ _ _ _ _ _ _
+
+________        
+|               
+|       |       
+
+
+Guess a letter 
+e
+_ e _ _ o _ _ _ _ _ _ _ _
+
+________        
+|               
+|       |       
+
+
+Guess a letter 
+h
+_ e _ _ o _ _ _ _ _ _ _ _
+
+________        
+|               
+|       |       
+|       O       
+
+
+Guess a letter 
+H
+H e _ _ o _ _ _ _ _ _ _ _
+
+________        
+|               
+|       |       
+|       O       
+
+
+Guess a letter 
+l
+H e l _ o _ _ _ _ _ _ _ _
+
+________        
+|               
+|       |       
+|       O       
+
+
+Guess a letter 
+l
+H e l l o _ _ _ _ _ _ _ _
+
+________        
+|               
+|       |       
+|       O       
+
+
+Guess a letter 
+i
+H e l l o _ _ _ _ _ _ _ _
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+
+
+Guess a letter 
+n
+H e l l o _ n _ _ _ _ _ _
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+
+
+Guess a letter 
+e
+H e l l o _ n _ e _ _ _ _
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+
+
+Guess a letter 
+t
+H e l l o _ n t e _ _ _ _
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+
+
+Guess a letter 
+t
+H e l l o _ n t e _ _ _ t
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+
+
+Guess a letter 
+n
+H e l l o _ n t e _ n _ t
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+
+
+Guess a letter 
+e
+H e l l o _ n t e _ n e t
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+
+
+Guess a letter 
+I
+H e l l o I n t e _ n e t
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+
+
+Guess a letter 
+r
+H e l l o I n t e r n e t
+
+________        
+|               
+|       |       
+|       O       
+|      /|\      
+You Win!
+H e l l o I n t e r n e t
+>>> 
